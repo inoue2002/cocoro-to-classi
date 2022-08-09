4f4e2638b35da4c41014b54c67e02e059f104970
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2022-08-09 23:25:49 +0900

    vercelにデプロイする設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   nitro: {
-    preset: "node",
+    preset: "vercel",
   },
   css: ["@/assets/css/tailwind.css"],
   // 参考:https://std9.jp/articles/01fq14pxsp4r6nbyzp2txj46ac
