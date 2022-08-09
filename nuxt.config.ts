4aa48768ab171b239837a83a6d3be9979f125118
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2022-08-09 22:30:52 +0900

    add config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
-import { defineNuxtConfig } from 'nuxt';
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   nitro: {
-    preset: 'node',
+    preset: "node",
   },
-  css: ['@/assets/css/tailwind.css'],
+  css: ["@/assets/css/tailwind.css"],
   // 参考:https://std9.jp/articles/01fq14pxsp4r6nbyzp2txj46ac
   build: {
     postcss: {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app.{vue,js,ts,jsx,tsx}", "./components/**/*.{vue,js,ts,jsx,tsx}", "./layouts/**/*.{vue,js,ts,jsx,tsx}", "./pages/**/*.{vue,js,ts,jsx,tsx}", "./plugins/**/*.{js,ts}"],
+  content: [
+    "./app.{vue,js,ts,jsx,tsx}",
+    "./components/**/*.{vue,js,ts,jsx,tsx}",
+    "./layouts/**/*.{vue,js,ts,jsx,tsx}",
+    "./pages/**/*.{vue,js,ts,jsx,tsx}",
+    "./plugins/**/*.{js,ts}",
+  ],
   theme: {
     extend: {},
   },
