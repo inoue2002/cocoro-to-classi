5818f146472aa3939ae06600969174c26ae80cc6
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2022-08-10 14:52:44 +0900

    ページを整える

---
+++ pages/event/index.vue
@@ -0,0 +1,25 @@
+<script setup>
+onMounted(async () => {
+  // ユーザーがログインしていない場合はログインページへ遷移
+  await useNuxtApp().$existCurrentUser();
+});
+
+const logOut = async () => {
+  await useAuth().signOut();
+  useRouter().push('/login');
+};
+</script>
+<template>
+  <div>
+    <div>
+      <p class="text-3xl">ログイン完了後</p>
+      <p>Hello!</p>
+      <div @click="logOut()">ログアウト</div>
+      <div>
+        ログイン情報
+        {{ useAuth().token }}
+      </div>
+    </div>
+    <NuxtWelcome />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+onMounted(async () => {
+  // ユーザーがログインしていない場合はログインページへ遷移、LPページを作成する場合はこの処理をなくす
+  await useNuxtApp().$existCurrentUser();
+});
+</script>
+<template>
+  <div>This page is LP</div>
+</template>
