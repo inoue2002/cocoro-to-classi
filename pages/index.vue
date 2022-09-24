377c95b5f58db984d1502fa02e984463994ce2ff
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2022-09-25 00:54:30 +0900

    連続リーダーモードのボタンを作成する

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
-// liff.Init()
+// import { Item } from '~/@types/item';
+// eslint-disable-next-line import/no-named-as-default
+
 onMounted(async () => {
-  await useNuxtApp().$liffInit('1657500508-Mvd69BKj');
+  const user = await useNuxtApp().$liffInit('1657500508-Mvd69BKj');
+  console.log({ user });
+  // ユーザーが登録したデータを配列で取得（item[]）
+  // const ItemList: Item[] = [];
 });
+
+const toScan = () => {
+  const liff = (window as any).liff
+  liff.openWindow({ url: 'cocoro-to-classi.vercel.app/scan', external: true });
+  console.log('起動しました')
+};
 </script>
 <template>
-  <div class="root">Hello</div>
+  <div>
+    <div class="root">Hello</div>
+
+    <!-- 連続レーダーモード -->
+    <button @click="toScan">連続リーダーモード</button>
+  </div>
 </template>
 
 <style scoped>
