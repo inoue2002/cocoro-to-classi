7f5eaa9559503f418a5fb386fdd6019f0d085989
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2022-09-25 00:53:53 +0900

    itemの型定義

---
+++ @types/item.d.ts
@@ -0,0 +1,16 @@
+export interface Item {
+  // アイテムの識別ID
+  id: string;
+  // 初期化された日付
+  init_at: Date;
+  // 設定された日付
+  register_at: Date;
+  // アイテムの名前
+  name: string;
+  // アイテムの画像URL
+  imageUrl: string;
+  // アイテムのメモ
+  message: string;
+  // 設定したユーザーのUserId
+  authorUserId: string;
+}
