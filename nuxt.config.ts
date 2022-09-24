aef50c377a2da049fdada5c5f32551cc98abf333
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2022-09-25 00:54:05 +0900

    不要なリンクを削除

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,6 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: 'cocoro-to-classi',
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/lp/favicon.png' }],
       meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
       script: [
         {
