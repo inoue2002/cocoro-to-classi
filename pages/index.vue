bb6654c58aedbcfd3d0966c07ee22314a5c1acce
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2022-09-25 00:02:59 +0900

    liffの初期化を行う

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
+// liff.Init()
+onMounted(async () => {
+  await useNuxtApp().$liffInit('1657500508-Mvd69BKj');
+  console.log(window.liff.getProfile());
+});
 </script>
 <template>
-  <div class="root">
-    Hello
-  </div>
+  <div class="root">Hello</div>
 </template>
 
 <style scoped>
-body, html {
+body,
+html {
   width: 100vw;
   height: 100vh;
 }
