ee1bca676908810baa63553da1244724008b7957
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2022-09-25 07:08:59 +0900

    新規登録画面

--- pages/register.vue
+++ pages/register.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
+import { Timestamp } from '@firebase/firestore';
 import { Ref } from 'vue';
 
+import { Item } from '~~/@types/item';
 import { LineUser } from '~~/@types/lineUser';
 
 const lineUser: Ref<LineUser> = ref();
 
 const imageFile: any = ref();
 
+const name = ref('');
+const message = ref('');
+
 // パラメーターの取得
 const route = useRoute();
 // todo - エラーハンドリングをやる
 const itemId = route.query.id as string;
+
+const router = useRouter();
 
 onMounted(async () => {
   lineUser.value = await useNuxtApp().$liffInit('1657500508-Mvd69BKj');
@@ -21,11 +28,24 @@ onMounted(async () => {
 
 const submit = async () => {
   const imageUrl = await useNuxtApp().$uploadfile(`item/${lineUser.value.userId}_${itemId}`, imageFile.value);
-  console.log({ imageUrl });
-
-  // useNuxtApp().$registerItem();
+  const item: Item = {
+    id: itemId,
+    register_at: Timestamp.fromDate(new Date()),
+    name: String(name.value),
+    imageUrl,
+    message: String(message.value),
+    authorUser: {
+      displayName: lineUser.value.displayName,
+      userId: lineUser.value.userId,
+      statusMessage: lineUser.value.statusMessage,
+      pictureUrl:
+        lineUser.value.pictureUrl ||
+        'https://firebasestorage.googleapis.com/v0/b/cocoro-to-classi.appspot.com/o/items%2Fitem%2F123456789_f4129065-9383-4b44-b0ad-9bc1ff1e16a8?alt=media&token=c504346b-a885-493b-999b-43b5e7ca27a0',
+    },
+  };
+  await useNuxtApp().$registerItem(item);
   console.log('登録しました');
-  // (window as any).liff.close();
+  router.push(`/item?id=${itemId}`);
 };
 
 const changeImageFile = (e: any) => {
@@ -53,6 +73,7 @@ const changeImageFile = (e: any) => {
       <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">ものの名前</label>
       <input
         id="titile"
+        v-model="name"
         type="text"
         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         required
@@ -62,6 +83,7 @@ const changeImageFile = (e: any) => {
       <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">メッセージ</label>
       <input
         id="message"
+        v-model="message"
         type="textarea"
         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         required
@@ -70,6 +92,8 @@ const changeImageFile = (e: any) => {
     <input type="file" accept="img/*,png/*" class="p-6 bg-red-500" @change="changeImageFile" />
     <div>
       <button @click="submit">送信</button>
+      {{ message }}
+      {{ name }}
     </div>
   </div>
 </template>
