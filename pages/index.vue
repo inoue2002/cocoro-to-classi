48241d402adabc7d3a9619821d305d9b30af8593
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2022-09-25 01:10:50 +0900

    連続リーダーモードの出現設定を整える

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,11 @@
 // import { Item } from '~/@types/item';
 // eslint-disable-next-line import/no-named-as-default
 
+const device = ref('');
+const NODE_ENV = process.env.NODE_ENV;
+
 onMounted(async () => {
+  device.value = (window as any).liff.getOS();
   const user = await useNuxtApp().$liffInit('1657500508-Mvd69BKj');
   console.log({ user });
   // ユーザーが登録したデータを配列で取得（item[]）
@@ -10,17 +14,16 @@ onMounted(async () => {
 });
 
 const toScan = () => {
-  const liff = (window as any).liff
+  const liff = (window as any).liff;
   liff.openWindow({ url: 'https://cocoro-to-classi.vercel.app/scan', external: true });
-  console.log('起動しました')
 };
 </script>
 <template>
   <div>
     <div class="root">Hello</div>
 
-    <!-- 連続レーダーモード -->
-    <button @click="toScan">連続リーダーモード</button>
+    <!-- 連続リーダーモード -->
+    <button v-if="device === 'android' || NODE_ENV === 'development'" @click="toScan">連続リーダーモード</button>
   </div>
 </template>
 
